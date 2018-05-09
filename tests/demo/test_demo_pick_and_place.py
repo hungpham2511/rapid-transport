63ba14f77794a4c72367822cc718973b2e51647d
Hung Pham <hungpham2511@gmail.com>
2018-05-09 20:31:25 +0800

    init demo script

---
+++ tests/demo/test_demo_pick_and_place.py
@@ -0,0 +1,31 @@
+import pytest, time
+import numpy as np
+from demos.pick_and_place import PickAndPlaceDemo
+
+import logging
+logging.basicConfig(level="DEBUG")
+
+
+@pytest.fixture(scope="module", params=[
+    ("scenarios/test0.scenario.yaml", "denso")
+])
+def setup_demo(request):
+    scene_name, robot_name = request.param
+    demo = PickAndPlaceDemo(scene_name)
+    assert demo.view()
+    yield demo, robot_name
+    demo.get_env().Destroy()
+
+
+def test_init(setup_demo):
+    demo, robot_name = setup_demo
+    env = demo.get_env()
+    assert env.GetRobot(robot_name) is not None
+    assert demo.get_robot() is not None
+    assert env.GetKinBody("object1") is not None
+    assert env.GetKinBody("object2") is not None
+
+
+def test_run(setup_demo):
+    demo, robot_name = setup_demo
+    assert demo.run()
